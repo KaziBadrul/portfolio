9bfa4b84de1ad22c2d2a4e195d0bccdb07b37edc
Kazi Badrul <47207000+KaziBadrul@users.noreply.github.com>
2026-06-09 22:44:05 +0600

    add new project

--- src/app/components/home/about-me/index.tsx
+++ src/app/components/home/about-me/index.tsx
@@ -1,34 +1,64 @@
 import { Badge } from "@/components/ui/badge";
 
 const AboutMe = () => {
-    const servicesBedge = ["Machine Learning", "Computer Vision", "Full-Stack Development", "Prompt Engineering", "Frontend Development", "Backend Development", "Database Design"];
-    return (
-        <section>
-            <div className="container">
-                <div className="border-x border-primary/10 bg-[url('/images/about-me/about-me-bg.svg')] bg-cover bg-center bg-no-repeat">
-                    <div className="flex flex-col gap-9 sm:gap-12 max-w-3xl mx-auto px-4 sm:px-7 py-11 md:py-20">
-                        <div className="flex flex-col gap-4">
-                            <p className="text-sm tracking-[2px] text-primary uppercase font-medium">About Me</p>
-                            <h2 className="text-xl sm:text-2xl md:text-3xl lg:text-[32px]">AI-focused software engineer with hands-on experience in <span className="bg-[linear-gradient(90deg,_rgba(243,202,77,0.4)_0%,_rgba(243,202,77,0.05)_100%)]">machine learning, computer vision, and full-stack development.</span></h2>
-                            <h5 className="text-secondary font-normal">Currently researching advanced learning techniques with faculty mentorship. Seeking roles to leverage technical depth and research background in solving complex problems.</h5>
-                        </div>
-                        <div className="flex flex-col gap-4">
-                            <p className="text-sm text-primary uppercase font-medium">Core Competencies</p>
-                            <div className="flex flex-wrap gap-2 sm:gap-3">
-                                {servicesBedge?.map((value, index) => {
-                                    return (
-                                        <Badge variant={"outline"} key={index} className="py-1.5 px-3 rounded-lg">
-                                            <p className="text-xs sm:text-sm font-medium text-primary">{value}</p>
-                                        </Badge>
-                                    )
-                                })}
-                            </div>
-                        </div>
-                    </div>
-                </div>
+  const servicesBedge = [
+    "Machine Learning",
+    "Computer Vision",
+    "Deep Learning",
+    "RAG",
+    "Multi-LLM",
+    "Full-Stack Development",
+    "Prompt Engineering",
+    "Frontend Development",
+    "Backend Development",
+    "Database Design",
+  ];
+  return (
+    <section>
+      <div className="container">
+        <div className="border-x border-primary/10 bg-[url('/images/about-me/about-me-bg.svg')] bg-cover bg-center bg-no-repeat">
+          <div className="flex flex-col gap-9 sm:gap-12 max-w-3xl mx-auto px-4 sm:px-7 py-11 md:py-20">
+            <div className="flex flex-col gap-4">
+              <p className="text-sm tracking-[2px] text-primary uppercase font-medium">
+                About Me
+              </p>
+              <h2 className="text-xl sm:text-2xl md:text-3xl lg:text-[32px]">
+                AI-focused software engineer with hands-on experience in{" "}
+                <span className="bg-[linear-gradient(90deg,_rgba(243,202,77,0.4)_0%,_rgba(243,202,77,0.05)_100%)]">
+                  machine learning, computer vision, and full-stack development.
+                </span>
+              </h2>
+              <h5 className="text-secondary font-normal">
+                Currently researching advanced learning techniques with faculty
+                mentorship. Seeking roles to leverage technical depth and
+                research background in solving complex problems.
+              </h5>
             </div>
-        </section>
-    )
-}
+            <div className="flex flex-col gap-4">
+              <p className="text-sm text-primary uppercase font-medium">
+                Core Competencies
+              </p>
+              <div className="flex flex-wrap gap-2 sm:gap-3">
+                {servicesBedge?.map((value, index) => {
+                  return (
+                    <Badge
+                      variant={"outline"}
+                      key={index}
+                      className="py-1.5 px-3 rounded-lg"
+                    >
+                      <p className="text-xs sm:text-sm font-medium text-primary">
+                        {value}
+                      </p>
+                    </Badge>
+                  );
+                })}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default AboutMe
+export default AboutMe;
